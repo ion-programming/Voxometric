df2f6a4e3049825a9ecb3c8eb726a8f2d5fd5592
Lucas Fraser <lucas@ionprogramming.com.au>
2015-08-01 15:59:33 +0930

    Update

--- Voxometric/src/au/com/ionprogramming/voxometric/blocks/Grass.java
+++ Voxometric/src/au/com/ionprogramming/voxometric/blocks/Grass.java
@@ -1,14 +1,18 @@
 package au.com.ionprogramming.voxometric.blocks;
 
+import org.newdawn.slick.Color;
+
 import au.com.ionprogramming.voxometric.Block;
-import au.com.ionprogramming.voxometric.Images;
 
 public class Grass extends Block {
 	
+	static Color cGrassTop = new Color(0, 128, 0);
+	static Color cGrassSides = new Color(100, 0, 0);
+	
 	public Grass(int x, int y, int z) {
 		super(x, y, z);
-	
-		setTexture(Images.topGrass, Images.lGrass, Images.rGrass);
+		setColor(cGrassTop, cGrassSides);
+//		setTexture(Images.topGrass, Images.lGrass, Images.rGrass);
 		
 	}
 
